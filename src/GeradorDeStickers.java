1c9df540632eeaaa3475b99c541ba2ff001932d4
William <willvsouza@gmail.com>
2023-04-07 19:02:13 -0300

    criacao da classe GeradorDeStickers

---
+++ src/GeradorDeStickers.java
@@ -0,0 +1,14 @@
+public class GeradorDeStickers {
+
+    public void cria() {
+
+        // fazer a leitura da imagem
+
+        // criar nova imagem em memória com transparência e redimensionada
+
+        // escrever uma frase na nova imagem
+
+        // copiar a imagem original para nova imagem (em memória)
+
+    }
+}
