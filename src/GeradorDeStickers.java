acc732a1188c7823ad57c1e6f454276b8b474f39
William <willvsouza@gmail.com>
2023-04-07 19:38:13 -0300

    criacao de imagem redimensionada e com transparencia

--- src/GeradorDeStickers.java
+++ src/GeradorDeStickers.java
@@ -1,14 +1,36 @@
+import javax.imageio.ImageIO;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class GeradorDeStickers {
 
-    public void cria() {
+    public void cria() throws IOException {
 
         // fazer a leitura da imagem
+        BufferedImage imagemOriginal = ImageIO.read(new File("entrada/filme.jpg"));
 
         // criar nova imagem em memória com transparência e redimensionada
+        int largura = imagemOriginal.getWidth();
+        int altura = imagemOriginal.getHeight();
+        int novaAltura = altura + 200;
+        BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+
+        // copiar a imagem original para nova imagem (em memória)
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal, 0, 0, null);
 
         // escrever uma frase na nova imagem
 
-        // copiar a imagem original para nova imagem (em memória)
 
+        // escrever a nova imagem num arquivo
+        ImageIO.write(novaImagem, "png", new File("saida/figurinha.png"));
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        GeradorDeStickers geradora = new GeradorDeStickers();
+        geradora.cria();
     }
 }
